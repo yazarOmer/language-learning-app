1a79a1f8f6e218cbbe82491ebcd3a77f2020cbf1
ömer <omeryazar0808@gmail.com>
2023-12-18 05:23:31 +0300

    feat: add authentication controller functions

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -8,6 +9,10 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
