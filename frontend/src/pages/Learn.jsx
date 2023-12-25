99cbc95e553a839dc76447276cea795e014fdf1d
ömer <omeryazar0808@gmail.com>
2023-12-26 00:16:49 +0300

    fix: fixed header position

--- frontend/src/pages/Learn.jsx
+++ frontend/src/pages/Learn.jsx
@@ -35,7 +35,7 @@ const Learn = () => {
 
     return (
         <div className="flex-1 items-center mx-auto mt-5 flex flex-col gap-3 ">
-            <div className="w-[600px]  flex items-center py-5 border-b-2 border-dark-border">
+            <div className="w-[600px] sticky top-0 bg-dark-bg flex items-center py-5 border-b-2 border-dark-border">
                 <NavLink to="/sections">
                     <svg
                         className="w-6 h-6 fill-dark-border"
