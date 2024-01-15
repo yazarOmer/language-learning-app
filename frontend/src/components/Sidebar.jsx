2b02431025b782bd38af0c4825302dc5bd1fd52d
ömer <omeryazar0808@gmail.com>
2024-01-15 16:12:55 +0300

    add mini leaderboard

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../components/Loading";
-import { getUserStats, resetActions } from "../features/actions/actionsSlice";
+import {
+    getUserStats,
+    getUsersByPoints,
+    resetActions,
+} from "../features/actions/actionsSlice";
 
 const Sidebar = () => {
-    const { gem, lifePoint, isLoading } = useSelector((state) => state.actions);
+    const { gem, lifePoint, isLoading, leaderboard } = useSelector(
+        (state) => state.actions
+    );
     const [time, setTime] = useState(0);
     const dispatch = useDispatch();
 
@@ -13,6 +19,7 @@ const Sidebar = () => {
     const fetchData = async () => {
         await dispatch(getUserStats());
         await dispatch(resetActions());
+        await dispatch(getUsersByPoints());
     };
 
     useEffect(() => {
@@ -28,7 +35,7 @@ const Sidebar = () => {
     }
 
     return (
-        <div className="fixed right-0 top-0 w-[400px] h-screen ml-auto flex flex-col border-l-2 py-6 px-3 border-dark-border">
+        <div className="fixed right-0 top-0 w-[400px] h-screen ml-auto flex flex-col border-l-2 py-6 px-5 border-dark-border">
             <div className="w-full px-5 flex justify-between items-center">
                 <div className="flex gap-2 items-center hover:bg-dark-border rounded-lg transition p-2">
                     <img src="/gem.svg" className="w-5 h-5" alt="" />
@@ -40,6 +47,23 @@ const Sidebar = () => {
                     <img src="/heart.svg" className="w-5 h-5" alt="" />
                     <span className="text-light-blue font-bold text-xl">
                         {lifePoint}
+                    </span>
+                </div>
+            </div>
+            <div className="border rounded-lg px-5 py-5 mt-3 border-dark-text-white/50">
+                <h2 className="text-dark-text-white font-bold">Puan Tablosu</h2>
+                <div className="flex justify-between items-center py-2 px-3 rounded-lg hover:bg-dark-border">
+                    <span className="text-dark-text-white/40">
+                        {leaderboard.map((lboard, i) =>
+                            lboard._id == user._id ? i + 1 : ""
+                        )}
+                        . Sıradasın
+                    </span>
+                    <span className="text-dark-text-white/80 font-bold">
+                        {leaderboard.map((lboard) =>
+                            lboard._id == user._id ? lboard.point : ""
+                        )}{" "}
+                        Puan
                     </span>
                 </div>
             </div>
--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -4,10 +4,12 @@ import {
     getUsersByPoints,
     resetActions,
 } from "../features/actions/actionsSlice";
+import Loading from "../components/Loading";
 
 const Leaderboard = () => {
     const dispatch = useDispatch();
-    const { leaderboard } = useSelector((state) => state.actions);
+    const { leaderboard, isLoading } = useSelector((state) => state.actions);
+
     const fetchUsers = async () => {
         await dispatch(getUsersByPoints());
         await dispatch(resetActions());
@@ -17,6 +19,10 @@ const Leaderboard = () => {
         fetchUsers();
     }, []);
 
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <div className="flex-1 items-center mx-auto mt-5 flex flex-col gap-3">
             <div className="mt-10">
@@ -24,7 +30,7 @@ const Leaderboard = () => {
                     Puan Tablosu
                 </h1>
                 {leaderboard.map((user, i) => (
-                    <div className="w-[450px] flex py-3 mb-2 rounded-2xl border border-dark-text-white/75 hover:bg-dark-border/75 items-center justify-between">
+                    <div className="w-[500px] flex py-3 mb-2 rounded-2xl border border-dark-text-white/75 hover:bg-dark-border/75 items-center justify-between">
                         <div className="flex items-center gap-5">
                             <span className="text-lg text-dark-text-white font-bold ml-5">
                                 {i + 1}
